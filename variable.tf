05c83b684e2eaabaa7c028949b44a8425175b093
rakeshdhundhara <143716231+rakeshdhundhara@users.noreply.github.com>
2024-05-09 19:49:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,11 +5,11 @@ variable "workflow" {
   type = any
 }
 variable "NEWRELIC_ACCOUNT_ID" {
-  type = any
+
 }
 variable "NEWRELIC_API_KEY" {
-  type = any
+
 }
 variable "NEWRELIC_REGION" {
-  type = any
+
 }
