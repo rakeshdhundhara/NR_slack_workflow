2002534fa2d9cc60b20a6e376402798f5480aebd
rakeshdhundhara <143716231+rakeshdhundhara@users.noreply.github.com>
2024-05-09 19:12:53 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,3 +4,12 @@ variable "channel1" {
 variable "workflow" {
   type = any
 }
+variable "NEWRELIC_ACCOUNT_ID" {
+  type = any
+}
+variable "NEWRELIC_API_KEY" {
+  type = any
+}
+variable "NEWRELIC_REGION" {
+  type = any
+}
