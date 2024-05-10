1a8d227efe3deaa3f58f49c6b26fe10e0582c22b
rakeshdhundhara <143716231+rakeshdhundhara@users.noreply.github.com>
2024-05-10 10:20:21 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,6 +6,15 @@ variable "workflow" {
 }
 
 
-variable "NEWRELIC_ACCOUNT_ID" {}
-variable "NEWRELIC_API_KEY" {}
-variable "NEWRELIC_REGION" {}
+variable "NEWRELIC_ACCOUNT_ID" {
+  description = "The New Relic account ID."
+}
+
+variable "NEWRELIC_API_KEY" {
+  description = "The New Relic API key."
+}
+
+variable "NEWRELIC_REGION" {
+  description = "The region where your New Relic account is hosted."
+}
+
